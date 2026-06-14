5ab82118b821c0d3e6622b9676a6b178e2d647e0
Ahian0 <quesadaguadamuzahiandanier@gmail.com>
2026-06-14 15:01:29 -0600

    Update timezone.py

--- app/utils/timezone.py
+++ app/utils/timezone.py
@@ -2,5 +2,15 @@
 from datetime import datetime, timezone, timedelta
 CR_TZ = timezone(timedelta(hours=-6))
 
+
 def now_cr():
     return datetime.now(CR_TZ)
+
+
+def utc_to_cr(dt):
+    """Convierte un datetime UTC a hora de Costa Rica (UTC-6)."""
+    if dt is None:
+        return None
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=timezone.utc)
+    return dt.astimezone(CR_TZ)
