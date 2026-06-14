ccddf27c981fe42d0c850fff42cd9f3abef9fe1d
Anmarin2473 <andrickmarin592@gmail.com>
2026-06-14 13:12:21 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -208,12 +208,7 @@ def create_app(config_name='default'):
     # Filtros Jinja personalizados
     from app.utils.filters import register_filters
     register_filters(app)
-    # Registrar filtros de Jinja
-    from app.utils.filters import init_filters
-    init_filters(app)
-
-    return app
-
+   
     return app
 
 
