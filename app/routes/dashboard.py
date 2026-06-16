6a78349e7ab9a847c178f82793f7a0c3cd33bc50
Andrick Marín <andrickmarin592@gmail.com>
2026-06-15 18:34:06 -0600

    Update dashboard.py

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -17,11 +17,6 @@ from app.utils.cache import cached_view
 
 
 from app.utils.cache import cached_view
-
-@dashboard_bp.route('/')
-@login_required
-@cached_view(seconds=45, by_user=True)
-def index():
     
 
 
